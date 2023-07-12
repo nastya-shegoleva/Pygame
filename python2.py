3629ef9be701e245adb07092c42dd1e4177c7f02
nastya-shegoleva <138816761+nastya-shegoleva@users.noreply.github.com>
2023-07-12 19:26:54 +0300

    Update python2_2.py

--- python2.py
+++ python2.py
@@ -1,61 +1,53 @@
+import sys
 import pygame
-
 
 color_rect = input()
 color_circle = input()
 pygame.init()
-WIDTH, HEIGHT = 800, 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+SIZE = WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
+start_circle = 0
+draw = False
+start = 0
+draw_circle = False
+screen2 = pygame.Surface(screen.get_size())
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            draw = True
+            start = event.pos
+        elif event.type == pygame.MOUSEMOTION:
+            if draw:
+                position = event.pos
+                width = position[0] - start[0]
+                height = position[1] - start[1]
+                pygame.draw.rect(screen, color_rect, (start[0], start[1], width, height))
+                screen2.blit(screen, (0, 0))
+                screen.fill((0, 0, 0))
+                screen.blit(screen2, (0, 0))
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+            draw = False
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 2:
+            draw_circle = True
+            start_circle = event.pos
+        elif event.type == pygame.MOUSEMOTION:
+            if draw_circle:
+                position = event.pos
+                radius = position[0] - start_circle[0] + position[1] - start_circle[1]
+                pygame.draw.circle(screen, color_circle, (start_circle[0], start_circle[1]), radius)
+                screen2.blit(screen, (0, 0))
+                screen.fill((0, 0, 0))
+                screen.blit(screen2, (0, 0))
+        elif event.type == pygame.MOUSEBUTTONUP and event.button == 2:
+            draw_circle = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_BACKSPACE:
+                screen.fill((0, 0, 0))
 
-class Cube:
-    def update(self):
-        self.x, self.y = pygame.mouse.get_pos()
-        self.square = pygame.Rect(self.x - 25, self.y - 25, 50, 50)
-
-    def draw_rect(self):
-        self.color = (100, 0, 255)
-        pygame.draw.rect(screen, color_rect, self.square)
-
-
-class Circle():
-    def update(self):
-        self.x, self.y = pygame.mouse.get_pos()
-
-    def draw_circle(self):
-        pygame.draw.circle(screen, color_circle, (self.x, self.y), 20)
-
-def main():
-    run = True
-    cubeList = []
-    circle_lst = []
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                cube = Cube()
-                cubeList.append(cube)
-                cube.update()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_1:
-                    circle = Circle()
-                    circle_lst.append(circle)
-                    circle.update()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_BACKSPACE:
-                    screen.fill((0, 0, 255))
-                    circle_lst.clear()
-                    cubeList.clear()
-        clock.tick(60)
-        screen.fill((0, 0, 255))
-        for x in cubeList:
-            x.draw_rect()
-        for x in circle_lst:
-            x.draw_circle()
-        pygame.display.flip()
-    pygame.quit()
-
-
-if __name__ == "__main__":
-    main()
+    pygame.display.update()
+    clock.tick(30)
